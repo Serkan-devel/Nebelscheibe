ffdf1af369f1427601e0572ed0b1c3368162555c
Serkan-devel <serkan.devel@gmail.com>
2018-11-04 11:18:44 +0100

    switching from flask to responder

--- maindrive.py
+++ maindrive.py
@@ -1,18 +1,21 @@
-from flask import Flask
+import responder
 from minds-api import minds-api as mds
 
-app = Flask(__name__)
+api = responder.API()
 
-@app.route('/')
+@api.route('/')
 def entry():
     pass
 
-@app.route('/media/<m-id>')
+@api.route('/media/{m-id}')
 
-@app.route('/newsfeed/<id>')
+@api.route('/newsfeed/{id}')
 
-@app.route('/blog/view/<b-id>')
+@api.route('/blog/view/{b-id}')
 
 @app.route('/user/<u-id>')
 
 @app.route('/group/profile/<g-id>')
+
+if __name__ == '__main__':
+    api.run()
