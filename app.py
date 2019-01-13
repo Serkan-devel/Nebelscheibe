cb07a81aef1e5bbb33c26e62e2f5501a1fb3157a
Serkan-devel <serkan.devel@gmail.com>
2019-01-13 12:10:42 +0100

    add templates

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
 from minds import Minds, Profile
 import uvicorn
 
 app = Starlette(debug=True, template_directory='templates')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
-@app.route('/')
-def entry():
-    pass
+@app.route('/{input}')
+def entry(input):
+    template = app.get_template('base.html')
+    content = template.render(page_title=input)
+    return HTMLResponse(content)
 
-@app.route('/{guid}')
+#@app.route('/{guid}')
 
-@app.route('/media/{guid}')
+#@app.route('/media/{guid}')
 
-@app.route('/newsfeed/{guid}')
+#@app.route('/newsfeed/{guid}')
 
-@app.route('/blog/view/{guid}')
+#@app.route('/blog/view/{guid}')
 
-@app.route('/user/{guid}')
+#@app.route('/user/{guid}')
 
-@app.route('/group/profile/{guid}')
+#@app.route('/group/profile/{guid}')
 
 if __name__ == '__main__':
     uvicorn.run(app)
