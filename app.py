825bdb4a0c9e2a3499672bc946b77c19fec6a375
Serkan-devel <serkan.devel@gmail.com>
2019-01-13 17:37:16 +0100

    begin implementing newsfeed endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from minds import Minds, Profile
 
 app = Flask(__name__)
+minds_api = Minds()
 
 @app.route('/<input>')
 def entry(input):
@@ -11,7 +12,10 @@ def entry(input):
 
 #@app.route('/media/{guid}')
 
-#@app.route('/newsfeed/{guid}')
+@app.route('/newsfeed/<guid>')
+def newsfeed(guid):
+    content = minds_api.newsfeed_single(guid,None,12)
+    return str(content)
 
 #@app.route('/blog/view/{guid}')
 
